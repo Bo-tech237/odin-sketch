186e61395f81fd91dda3f64f4cec425a72178b90
Ferdinand <bokallitechnology@gmail.com>
2022-09-02 12:41:38 +0100

    Add reset button and createGrid function

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-const maincontainer = document.getElementById('container');
-let sketchDiv = document.createElement('div');
-sketchDiv.classList.add('sketch-container');
+const mainContainer = document.getElementById('container');
+const progressBar = document.getElementById('my-range');
+const gridSize = document.querySelector('.grid-size');
+const resetBtn = document.querySelector('.reset-btn');
 
-for(i = 0; i < 16; i++) {
-
-    let div = document.createElement('div');
-    div.setAttribute('class', 'box');
-    sketchDiv.appendChild(div);
+function createGrid(e) {
     
-}
+    mainContainer.innerHTML = "";
+    let progressBarValue = e.target.value;
+    gridSize.innerText = `Grid size: ${progressBarValue} x ${progressBarValue}`;
+    let sketchContainer = document.createElement('div');
+    sketchContainer.classList.add('sketch-container');
+    sketchContainer.style.setProperty("grid-template-columns", `repeat(${progressBarValue}, 1fr)`);
+    for(i = 0; i < progressBarValue**2; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'square');
+        sketchContainer.appendChild(div);
+        mainContainer.appendChild(sketchContainer); 
+    }
+    
+    let sketchChildren = [];
+    sketchChildren = Array.from(sketchContainer.children);
+    
+    sketchChildren.forEach(sketchChild => {
+        sketchChild.addEventListener('mousemove', function(e) {
+            e.target.style.backgroundColor = "blue";
+        });
+    });
 
-maincontainer.append(sketchDiv);
-console.log(maincontainer);
+};
+
+
+progressBar.addEventListener('click',createGrid);
+
+resetBtn.addEventListener('click', function() {
+    mainContainer.innerHTML = "";
+    progressBar.value = 0;
+    gridSize.innerText = "";
+});
